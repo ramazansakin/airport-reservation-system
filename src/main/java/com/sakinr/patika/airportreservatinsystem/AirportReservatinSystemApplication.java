06bd9da38497be15f289f4c4e13ebe55adc2b61d
Ramazan Sakin <ramazansakin63@gmail.com>
2022-01-21 13:11:10 +0300

    project initialized with Readme

---
+++ src/main/java/com/sakinr/patika/airportreservatinsystem/AirportReservatinSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sakinr.patika.airportreservatinsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportReservatinSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportReservatinSystemApplication.class, args);
+	}
+
+}
