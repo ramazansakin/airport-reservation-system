a3e23f3e9bb93b9ce6f3df334c15b7e3c8e93c8f
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-12 21:27:24 +0300

    local/test profiles defined for security config & props up

--- src/main/java/com/sakinr/patika/airportreservatinsystem/config/WebSecurityConfigLocal.java
+++ src/main/java/com/sakinr/patika/airportreservatinsystem/config/WebSecurityConfigLocal.java
@@ -1,8 +1,5 @@
 package com.sakinr.patika.airportreservatinsystem.config;
 
-import com.sakinr.patika.airportreservatinsystem.security.JwtTokenFilterConfigurer;
-import com.sakinr.patika.airportreservatinsystem.security.JwtTokenProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -11,41 +8,20 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-@Profile("test")
+@Profile("local")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private JwtTokenProvider jwtTokenProvider;
+public class WebSecurityConfigLocal extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        // Disable CSRF (cross site request forgery)
-        http.csrf().disable();
-
-        // No session will be created or used by spring security
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
         // Entry points
         http.authorizeRequests()
-                .antMatchers("/users/signin").permitAll()
-                .antMatchers("/users/signup").permitAll()
-                .antMatchers("/actuator/**").permitAll()
-                .antMatchers("/api/airport-company/**").permitAll()
-//                .antMatchers("/api/flight/**").hasRole("ADMIN")
-                // Disallow everything else..
-                .anyRequest().authenticated();
-
-        // Apply JWT
-        http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
+                .anyRequest().permitAll();
 
     }
 
@@ -59,5 +35,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
 }
