459d67d4b43780136677b67c8ea6a87d76358c0e
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-13 19:48:47 +0300

    test dep added

--- src/main/java/com/sakinr/patika/airportreservatinsystem/config/WebSecurityConfigLocal.java
+++ src/main/java/com/sakinr/patika/airportreservatinsystem/config/WebSecurityConfigLocal.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Profile("local")
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfigLocal extends WebSecurityConfigurerAdapter {
 
     @Override
