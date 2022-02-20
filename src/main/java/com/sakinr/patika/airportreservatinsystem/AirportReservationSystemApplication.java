1f3d693159021c044aff631d13a10144828d9a1b
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-20 07:18:06 +0300

    swagger2 enabled

--- src/main/java/com/sakinr/patika/airportreservatinsystem/AirportReservationSystemApplication.java
+++ src/main/java/com/sakinr/patika/airportreservatinsystem/AirportReservationSystemApplication.java
@@ -2,12 +2,26 @@ package com.sakinr.patika.airportreservatinsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AirportReservationSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AirportReservationSystemApplication.class, args);
+    }
+
+    // Swagger api documentation config for base package
+    @Bean
+    public Docket documentApis() {
+        return new Docket(DocumentationType.SWAGGER_2).select().apis(
+                RequestHandlerSelectors.basePackage("com.sakinr.patika.airportreservationsystem.controller")
+        ).build();
     }
 
 }
