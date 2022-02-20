dddbe4b27971c25818b45de0b64a5bb18188fcf5
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-20 09:46:37 +0300

    clean up

--- src/main/java/com/sakinr/patika/airportreservatinsystem/AirportReservationSystemApplication.java
+++ src/main/java/com/sakinr/patika/airportreservatinsystem/AirportReservationSystemApplication.java
@@ -2,10 +2,8 @@ package com.sakinr.patika.airportreservatinsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class AirportReservationSystemApplication {
 
     public static void main(String[] args) {
