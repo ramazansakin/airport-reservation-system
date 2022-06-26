5289f5580e24edc79400a9d4c0274076536b390c
Ramazan Sakin <ramazansakin63@gmail.com>
2022-06-26 14:31:38 +0300

    profile setting for tests

--- src/test/java/com/sakinr/patika/airportreservationsystem/AirportReservatinSystemApplicationTests.java
+++ src/test/java/com/sakinr/patika/airportreservationsystem/AirportReservatinSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.sakinr.patika.airportreservationsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class AirportReservatinSystemApplicationTests {
 
 	@Test
