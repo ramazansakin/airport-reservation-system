f1ec38cea37d82532daa09525de3d743de80e2aa
Ramazan Sakin <ramazansakin63@gmail.com>
2022-08-10 07:55:22 +0300

    feat: Passenger Mapper copyProps func added

--- src/main/java/com/sakinr/patika/airportreservationsystem/model/mapper/PassengerMapper.java
+++ src/main/java/com/sakinr/patika/airportreservationsystem/model/mapper/PassengerMapper.java
@@ -1,8 +1,12 @@
 package com.sakinr.patika.airportreservationsystem.model.mapper;
 
-import com.sakinr.patika.airportreservationsystem.model.entity.Passenger;
 import com.sakinr.patika.airportreservationsystem.model.dto.PassengerDTO;
+import com.sakinr.patika.airportreservationsystem.model.entity.Passenger;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 // Sample mapstruct defined mapper
 @Mapper
@@ -10,4 +14,9 @@ public interface PassengerMapper {
     PassengerDTO toDto(Passenger entity);
 
     Passenger toEntity(PassengerDTO dto);
+
+    @BeanMapping(
+            nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+            nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+    void fill(Passenger source, @MappingTarget Passenger target);
 }
