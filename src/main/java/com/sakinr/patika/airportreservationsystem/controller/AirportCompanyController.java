d88ee2e62fd962d4a2602b5328e0303b258a980b
Ramazan Sakin <ramazansakin63@gmail.com>
2022-10-21 10:41:07 +0300

    chore: clean up

--- src/main/java/com/sakinr/patika/airportreservationsystem/controller/AirportCompanyController.java
+++ src/main/java/com/sakinr/patika/airportreservationsystem/controller/AirportCompanyController.java
@@ -1,7 +1,6 @@
 package com.sakinr.patika.airportreservationsystem.controller;
 
 
-import com.sakinr.patika.airportreservationsystem.exception.QuotaIsFullException;
 import com.sakinr.patika.airportreservationsystem.model.entity.AirportCompany;
 import com.sakinr.patika.airportreservationsystem.model.entity.Flight;
 import com.sakinr.patika.airportreservationsystem.model.entity.Ticket;
@@ -9,10 +8,18 @@ import com.sakinr.patika.airportreservationsystem.service.AirportCompanyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
@@ -68,15 +75,10 @@ public class AirportCompanyController {
     }
 
     @PostMapping(value = "/buy-ticket")
-    public ResponseEntity<?> buyTicket(@RequestParam @Min(1) Integer flightId, @RequestParam @Min(1) Integer passengerId) {
-        ResponseEntity<?> response;
-        try {
-            Ticket ticket = airportCompanyService.buyTicketForFlight(flightId, passengerId);
-            response = new ResponseEntity<>(ticket, HttpStatus.OK);
-        } catch (QuotaIsFullException exception) {
-            response = new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        return response;
+    public ResponseEntity<?> buyTicket(@RequestParam @Min(1) final Integer flightId,
+                                       @RequestParam @Min(1) final Integer passengerId) {
+        Ticket ticket = airportCompanyService.buyTicketForFlight(flightId, passengerId);
+        return new ResponseEntity<>(ticket, HttpStatus.OK);
     }
 
     @PostMapping(value = "/cancel-ticket")
